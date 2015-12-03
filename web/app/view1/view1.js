b0eae8bf83d528cc38c57d4de2c5001c9e5c9b37
Bo Vilstrup Mortensen <bo.vilstrup@gmail.com>
2015-12-03 13:25:32 +0100

    changes to view1

--- web/app/view1/view1.js
+++ web/app/view1/view1.js
@@ -50,65 +50,14 @@ angular.module('myApp.view1', ['ngRoute'])
 
                 $http({
                     method: 'GET',
-                    url: 'api/flightinfo/info'
-//                            + $scope.dataFrom.repeatSelectFrom + '/'
-//                            + $scope.dataTo.repeatSelectTo + '/'
-//                            + $scope.dDate.toISOString() + '/'
-////                                + $scope.dDate + '/'
-////                                + $scope.fDate + '/'
-//                            + $scope.passengers
+                    url: 'api/flightinfo/'
+                            + $scope.dataFrom.repeatSelectFrom + '/'
+                            + $scope.dataTo.repeatSelectTo + '/'
+                            + $scope.dDate.toISOString() + '/'
+                            + $scope.passengers
 
                 }).then(function successCallback(response) {
                    $scope.airlines = response.data;
-
-//                    $scope.airlines = [
-//                        {// First airline
-//                            "airline": "First airline",
-//                            "flights": [
-//                                {
-//                                    "flightID": "COL3256",
-//                                    "numberOfSeats": 2,
-//                                    "date": "2016-01-04T10:00:00.000Z",
-//                                    "totalPrice": 130,
-//                                    "traveltime": 90,
-//                                    "origin": "CPH",
-//                                    "destination": "STN"
-//                                },
-//                                {
-//                                    "flightID": "COL3256",
-//                                    "numberOfSeats": 2,
-//                                    "date": "2016-01-04T19:00:00.000Z",
-//                                    "totalPrice": 100,
-//                                    "traveltime": 90,
-//                                    "origin": "CPH",
-//                                    "destination": "STN"
-//                                }
-//                            ]
-//                        }, // End of First
-//                        {// second airline
-//                            "airline": "second airline",
-//                            "flights": [
-//                                {
-//                                    "flightID": "COL3256",
-//                                    "numberOfSeats": 2,
-//                                    "date": "2016-01-04T10:00:00.000Z",
-//                                    "totalPrice": 130,
-//                                    "traveltime": 90,
-//                                    "origin": "CPH",
-//                                    "destination": "STN"
-//                                },
-//                                {
-//                                    "flightID": "COL3256",
-//                                    "numberOfSeats": 2,
-//                                    "date": "2016-01-04T19:00:00.000Z",
-//                                    "totalPrice": 100,
-//                                    "traveltime": 90,
-//                                    "origin": "CPH",
-//                                    "destination": "STN"
-//                                }
-//                            ]
-//                        } // End of Second airline
-//                    ]; // End of airlines
 
                 }, function errorCallback(response) {
                     alert("Error occured");
