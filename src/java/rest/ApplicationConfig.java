591a99f4da5789e4dfe8efcd3cddfd21a9c0f97e
Bo Vilstrup Mortensen <bo.vilstrup@gmail.com>
2015-12-14 10:41:28 +0100

    applicationConfig.java added

--- src/java/rest/ApplicationConfig.java
+++ src/java/rest/ApplicationConfig.java
@@ -23,8 +23,8 @@ public class ApplicationConfig extends Application {
   private void addRestResourceClasses(Set<Class<?>> resources) {
     resources.add(rest.Admin.class);
     resources.add(rest.FlightinfoResource.class);
+    resources.add(rest.Reservations.class);
     resources.add(rest.User.class);
-    resources.add(rest.reservations.class);
     resources.add(security.JWTAuthenticationFilter.class);
     resources.add(security.Login.class);
         resources.add(security.NotAuthorizedExceptionMapper.class);
